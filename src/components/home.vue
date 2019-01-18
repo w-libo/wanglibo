52d183268acad901e7644da7e1de09d037e04000
王李波 <wlbwtl@163.com>
2019-01-18 16:07:29 +0800

    首页增加banner轮播

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,25 +1,68 @@
 <template>
+
   <div id="home">
-    <router-link to="/ShowBlogs">
-      <button class="btn btn-default">点击跳转</button>
-    </router-link>
-    
-    
+    <template>
+      <el-carousel :interval="2000" arrow="hover">
+        <el-carousel-item v-for="item in superurl">
+          <router-link :to="item.url"><img :src="item.img"/></router-link>
+        </el-carousel-item>
+      </el-carousel>
+    </template>
+
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'home',
+
   data () {
     return {
+      superurl: [
+        {
+          url: '/ShowBlogs',
+          img: require('../assets/images/wlb2.png'),
+          title: '你看我叼吗1',
+          id: 1
+        },
+        {
+          url: '',
+          img: require('../assets/images/wlb3.png'),
+          title: '你看我叼吗1',
+          id: 1
+        },
+        {
+          url: '',
+          img: require('../assets/images/wlb4.png'),
+          title: '你看我叼吗1',
+          id: 1
+        }
+      ]
 
     }
   },
+  mounted () {
 
-  
+  }
+
 }
 </script>
 
-<style>
+<style scoped>
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 18px;
+    opacity: 0.75;
+    line-height: 300px;
+    margin: 0;
+  }
+
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,30 +1,34 @@
 /* eslint-disable eqeqeq */
 /* eslint-disable quotes */
-// The Vue build version to load with the `import` command
+// 使用“import”命令加载的Vue生成版本
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
-import { Select, Option } from 'element-ui' // 引入element-ui框架
+import { Select, Option, Carousel, CarouselItem} from 'element-ui' // 引入element-ui框架
+
 import App from './App'
 import Routes from './routes'
 import axios from 'axios'
+
+// 全局样式引入
 import './assets/common/blogHeader.css' /* 引入公共样式 */
-import './assets/css/element.css'
+import './assets/css/element.css' /* 引入element-ui样式 */
+import 'swiper/dist/css/swiper.css'
 // 全局配置
 axios.defaults.baseURL = 'https://wd2895962302enkyee.wilddogio.com'
+// element-ui框架使用
 Vue.use(Select)
 Vue.use(Option)
+Vue.use(Carousel)
+Vue.use(CarouselItem)
+
 Vue.use(VueResource)
 Vue.use(VueRouter)
 Vue.config.productionTip = false
 
-// 自定义指令
-// Vue.directive('rainbow',{
-//   bind(el,binding,vnode){
-//     el.style.color = "#" + Math.random().toString(16).slice(2,8);
-//   }
-// })
+// 自定义指令内容自适应
 Vue.directive('theme', {
   bind (el, binding, vnode) {
     if (binding.value == 'wide') {
@@ -39,11 +43,8 @@ Vue.directive('theme', {
     }
   }
 })
-// 自定义过滤器
-// Vue.filter("to-uppercase",function(value){
-//   return value.toUpperCase();
-// })
 
+// 自定义过滤器标题英文大写
 Vue.filter('snippet', function (value) {
   return value.slice(0, 100) + "..."
 })
@@ -54,7 +55,7 @@ const router = new VueRouter({
   mode: "history"
 })
 
-/* eslint-disable no-new */
+// vue初始化
 new Vue({
   el: '#app',
   components: { App },
